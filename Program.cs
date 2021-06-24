4cba0cfab1cb919b9204ea387cd619830cdb740f
shalem0707 <86397896+shalem0707@users.noreply.github.com>
2021-06-24 19:42:54 +0530

    Add files via upload
    average of numbers

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace cSharp
+{
+    class Program
+    {
+      static int i,n,sum=0;
+       
+      public static void Main(String[] args)
+        { 
+         Console.WriteLine("Enter a number for pushing how many numbers");
+         int a=int.Parse(Console.ReadLine());
+         for(i=0;i<=a;i++){ 
+           n=int.Parse(Console.ReadLine());
+         sum=sum+n;
+         }
+         int div=sum/a;
+         Console.WriteLine(div);
+         }
+    }    
+         
+  
+
+}
